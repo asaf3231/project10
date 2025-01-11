2413844dcb4b3f768cadc13c2d35117b815ee294
Asaf Ramati <asaf.ramati@runi.co.il>
2025-01-11 10:02:11 +0200

    added compilationEngine API

---
+++ CompilationEngine.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CompilationEngine {
+
+    private static BufferedReader reader;
+
+
+    public CompilationEngine (File file) throws IOException{
+        reader = new BufferedReader(new FileReader(file));
+    }
+    
+    public static void compileClass() throws IOException {
+        
+    }
+
+    public static void compileClassVarDec() throws IOException {
+        
+    }
+
+
+    public static void compileSubroutine() throws IOException {
+        
+    }
+
+
+    public static void compileParamaterList() throws IOException {
+        
+    }
+
+
+    public static void compileSubroutineBody() throws IOException {
+        
+    }
+
+
+    public static void complieVarDec() throws IOException {
+        
+    }
+
+
+    public static void compileStatements() throws IOException {
+        
+    }
+
+
+}
